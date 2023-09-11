a94ddc0adecd29d9bfcb0ee84ea06157b18d3693
shubham-bhutada <137187942+shubham-bhutada@users.noreply.github.com>
2023-09-11 14:52:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let i=0, j=1, n=str.length();
+	while (i < n) {
+		count=1;
+		while(j < n) {
+		if(str.charAt(i) == str.charAt(j)) {
+			count++;
+			j++;
+			
+		} else { 
+			if(count == 1)
+			{
+				return str.charAt(i);
+			}
+			i = j;
+			count = 1;
+		}
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
